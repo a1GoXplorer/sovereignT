c7a16613801197081049913378cc47706cbe1296
Mark Daniels <mark_daniels1982@hotmail.com>
2014-12-15 11:54:39 -0800

    init sequelize

--- app.js
+++ app.js
@@ -9,3 +9,28 @@ var app = express();
 app.set('view-engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// the root route
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+//the about route
+app.get('/about', function (req, res) {
+	res.render('about');
+});
+
+//the login route
+app.get('/login', function (req, res) {
+	res.render('login');
+});
+
+//the signup route
+app.get('/signup', function (req, res) {
+	res.render('signup');
+});
+
+//checks to if server is listening to requests
+app.listen(3000, function () {
+  console.log("Listening");
+});
