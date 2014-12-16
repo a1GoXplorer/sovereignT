feec8e15c4e8c12bf805f140970cda4cdaca7bfa
Mark Daniels <mark_daniels1982@hotmail.com>
2014-12-16 10:45:28 -0800

    display data on page

--- README.md
+++ README.md
@@ -4,6 +4,18 @@ SovereignT is an application built to help educate people on the most important 
 //Flags
 http://www.iconarchive.com/artist/gosquared.html
 
-//Population query
+//Population queries
+(Brazil)
 curl -G http://api.worldbank.org/countries/br/indicators/SP.POP.TOTL?format=JSON
 
+(Russia)
+curl -G http://api.worldbank.org/countries/ru/indicators/SP.POP.TOTL?format=JSON
+
+(India)
+curl -G http://api.worldbank.org/countries/in/indicators/SP.POP.TOTL?format=JSON
+
+(China)
+curl -G http://api.worldbank.org/countries/cn/indicators/SP.POP.TOTL?format=JSON
+
+(Turkey)
+curl -G http://api.worldbank.org/countries/tr/indicators/SP.POP.TOTL?format=JSON
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var pg = require('pg');
+var request = require('request');
 
 var app = express();
 
@@ -15,28 +16,36 @@ app.get('/', function (req, res) {
   res.render('site/home');
 });
 
+app.get('/results', function (req, res) {
+  request('http://api.worldbank.org/countries/br/indicators/NY.GDP.MKTP.PP.CD?format=JSON', function(err,resp,body) {
+    var data = JSON.parse(body)[1];
+    res.render('site/results', {comparisonList: data});
+    });
+});
+
 //about route
 app.get('/about', function (req, res) {
-	res.render('site/about');
+  res.render('site/about');
 });
 
 //login route
 app.get('/login', function (req, res) {
-	res.render('site/login');
+  res.render('site/login');
 });
 
 //signup route
 app.get('/signup', function (req, res) {
-	res.render('site/signup');
+  res.render('site/signup');
 });
 
 //user_profile route
-app.get('/user_profile', function (req, res) {
-	res.render('site/user_profile');
+app.get('/user-profile', function (req, res) {
+  res.render('site/user_profile');
 });
 
-//checks to if server is listening to requests
-app.listen(3000, function () {
-  console.log(new Array('*').join());
-  console.log("Listening");
-});
+
+// checks to if server is listening to requests
+ app.listen(3000, function () {
+   console.log(new Array('*').join());
+   console.log("Listening");
+ });
